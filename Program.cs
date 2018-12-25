91050cf0a8eb540ffe9b01d037821b19cf44ee4d
dotnet1612 <46135939+dotnet1612@users.noreply.github.com>
2018-12-25 19:35:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2512
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            // int short byte double float long double string boolean?
+
+            int a, b;
+
+            Console.WriteLine("Please write your name:");
+
+            string name = Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.WriteLine(name);
+
+            Console.Write("Please enter 1st number:" );
+            // 1
+            //string stringNumber = Console.ReadLine();
+            //a = Convert.ToInt32(stringNumber);
+
+            // 2
+            a = Convert.ToInt32( Console.ReadLine() );
+
+            Console.Write("Please enter 2nd number:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+            Console.Write("Sum is : ");
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+
+        }
+    }
+}
